e50a805d3e5b5a0048ab048a92698750bcdb5ec6
chaitanya-lakkundi <chaitanya.lakkundi@gmail.com>
2019-04-01 21:01:28 +0530

    add percentage and preloader

--- Extension/sotagger.js
+++ Extension/sotagger.js
@@ -15,8 +15,8 @@ function get_progress_element(prob) {
 }
 
 function argsort(test) {
-	var result = [];
-	for(var i = 0; i != test.length; ++i) result[i] = i;
+	let result = [];
+	for(let i = 0; i != test.length; ++i) result[i] = i;
 	result = result.sort(function(u,v) { return test[u] - test[v]; })
 	return result;
 }
@@ -32,6 +32,7 @@ function display(proba, order) {
 
 	let tag_row = document.createElement("tr");
 	let progress_row = document.createElement("tr");
+	let percent_row = document.createElement("tr");
 
 	grid.append(document.createElement("br"));
 	grid.append(document.createElement("br"));
@@ -40,10 +41,13 @@ function display(proba, order) {
 
 		let tag_data = document.createElement("td");
 		let prog_data = document.createElement("td");
+		let perc_data = document.createElement("td");
 		
 		let boldface = document.createElement("strong");
 		let tag1 = document.createElement("a");		
-		
+		let perc_val = document.createElement("p");
+		perc_val.setAttribute("style", "font-size: 14px;");
+
 		tag1.setAttribute("class", "post-tag inactiveLink custom-tag" + i);
 		tag1.setAttribute("href", "#!");
 
@@ -55,23 +59,32 @@ function display(proba, order) {
 		tag_data.append(boldface);
 		tag_row.append(tag_data);
 
+		perc_val.innerText = Math.round(proba[idx]*100);
+		perc_data.append(perc_val);
+
 		let ele = get_progress_element(proba[idx]);
 		prog_data.append(ele);
 		progress_row.append(prog_data);
+		percent_row.append(perc_data);
 
 		if (i == 3) {
-			break;
+			break;			
 			// remove break if all 6 topics are to be displayed
+
 			newtab.append(tag_row);
 			newtab.append(progress_row);
+			newtab.append(percent_row);
+
 			// defining new rows
 			tag_row = document.createElement("tr");
 			progress_row = document.createElement("tr");
+			percent_row = document.createElement("tr");		
 		}
 	}
 
 	newtab.append(tag_row);
 	newtab.append(progress_row);
+	newtab.append(percent_row);
 
 	grid.append(newtab);
 }
@@ -86,13 +99,22 @@ function main() {
 	const post_title = document.getElementById("question-header").innerText;
 	const post_text = document.getElementById("question").getElementsByClassName("post-text")[0].innerText;
 
+	const grid = document.getElementsByClassName("grid ps-relative d-block")[0];
+	const preloader = document.createElement("div");
+	preloader.setAttribute("id", "mypreloader");
+
+	preloader.innerHTML = "<br/><h1><strong>Fetching Tags ...</strong></h1><br/>";
+
+	grid.append(preloader);
+
 	$.get(query_url+encodeURIComponent(post_title+post_text).substr(0,4000) , function(data){
 		let proba = eval(data);
 		process(proba);
+	})
+	.then(() => {
+		document.getElementById("mypreloader").innerHTML = "";
 	});
 }
-
-// why shouldn't we consider only top two lines and bottom two lines in posts?
 
 $(document).ready(function() {
 	main();
